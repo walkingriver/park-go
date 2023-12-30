6529802469caf8c73ee66276bc3497323ac4b351
Michael Callaghan <mikecallaghan67@gmail.com>
2023-12-30 15:41:41 -0500

    Specifically importing Ionic Components

--- src/app/bingo/bingo.page.ts
+++ src/app/bingo/bingo.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BingoCardService } from 'src/app/bingo-card.service';
 import { addIcons } from 'ionicons';
 import { home, refresh } from 'ionicons/icons';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonGrid, IonRow, IonCol, IonAlert } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-bingo',
   templateUrl: './bingo.page.html',
   styleUrls: ['./bingo.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
+  imports: [CommonModule, FormsModule, RouterLink,
+    IonAlert,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonButtons,
+    IonButton, IonIcon,
+    IonGrid, IonRow, IonCol]
 })
 export class BingoPage implements OnInit {
   private route = inject(ActivatedRoute);
